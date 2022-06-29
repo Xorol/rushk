e65169959874e704e36082e0b60d2b371686a6e7
Xorol <88636352+Xorol@users.noreply.github.com>
2022-06-28 17:21:19 -0700

    Big refactor (#8)

--- utils/time.py
+++
@@ -1,9 +0,0 @@
-import datetime as dt
-
-TIMESTAMP_FORMAT = "%H:%M:%S"
-
-def get_formatted_time():
-  """
-  Returns the currrent UTC hour, minute, and second formatted according to `TIMESTAMP_FORMAT`
-  """
-  return dt.datetime.now().strftime(TIMESTAMP_FORMAT)
