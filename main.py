5ecdf24a340978bd731801cd09f1b3434b724498
Xorol <88636352+Xorol@users.noreply.github.com>
2023-06-30 01:20:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,91 +1,38 @@
-###########
-#LIBRARIES#
-###########
+import interactions
+from os import environ
+import utils
+import random
+import asyncio
 
-import interactions  # discord api wrapper
-import os  # You know what this is
-import utils  # custom module, in folder named utils
-import random  # You also know what this is
-import logging  # Helps with debugging
-from interactions.ext.tasks import IntervalTrigger, create_task  # Task extension
-from interactions.ext.wait_for import setup # Wait-for extension
+bot = interactions.Client(delete_unused_application_cmds=True)
 
-#######
-#DEBUG#
-#######
-
-logging.basicConfig(level=logging.WARNING)
-
-#####
-#BOT#
-#####
-
-bot: interactions.Client = interactions.Client(
-    token=os.environ["token"],  # Token secret
-    #disable_sync = True
-)
-
-# Comes from the wait_for extension,
-# Applies hooks to the class
-setup(bot)
-
-bot_name = "Rushk but hip"
-
-##################
-#REVOLVING STATUS#
-##################
-
-# List of available statuses (statii?) to rotate through
-with open("statuses.txt", "r") as f:
-  status_wheel = f.readlines()
+dicts = utils.load_dictionaries()
+word_amt = 0
+for dict in dicts:
+  word_amt += utils.Dictionary(dict, dicts).length
+dict_amt = str(len(dicts))
+word_amt = str(word_amt)
+del dicts
 
 
-# Makes it switch statuses every 15 seconds
-@create_task(IntervalTrigger(15))
-async def switch_statuses():
-    status = random.choice(status_wheel)
-    await utils.set_game(status, bot)
+with open("extensions.txt", "r") as extf:
+      EXTENSIONS = extf.read().splitlines(False)
+
+with open("statuses.txt", "r") as statuses_file:
+  statuses = statuses_file.readlines()
+
+@interactions.Task.create(interactions.IntervalTrigger(seconds=15)) 
+async def set_status():
+    new_status = random.choice(statuses).replace("{words}", word_amt).replace("{dictamt}", dict_amt)
+    await utils.set_game(new_status, bot)
+
+@interactions.listen()
+async def on_startup():
+    print("Bot is ready!")
+    set_status.start()
+
+for i in EXTENSIONS:
+  bot.load_extension(i)
 
 
-##########
-#ON READY#
-##########
-
-
-@bot.event()
-async def on_ready():
-
-    # Get all guilds the bot is in
-    guilds = bot.guilds
-
-    # Print join message
-    utils.ts_print(
-        f"[%s] Joining {guilds[0]} as {bot_name}!"
-    )
-
-    # Set an initial status
-    #await utils.set_game(
-    #    "You've seen me within 15 seconds of me starting up! You must be a true fan!",
-    #    bot
-    #    )
-
-    # Begin the status revolver
-    #switch_statuses.start()
-    utils.ts_print("[%s] Status revolver started!")
-
-
-##############################
-#COMMAND & BOT INITIALIZATION#
-##############################
-
-exts = []
-for filename in os.listdir(os.path.join(os.path.dirname(os.path.realpath(__file__)), "commands")):
-    if filename.endswith(".py"):
-        exts.append(filename.replace(".py", ""))
-
-for i in exts:
-    utils.ts_print(f"[%s] Loaded extension '{i}'")
-    bot.load(f"commands.{i}")
-
-# Start the bot
-bot.start()
+bot.start(environ["TOKEN"])
