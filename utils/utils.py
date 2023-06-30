3abeb1e3cb21e23004f46042f8bcf1c333be2cb8
Xorol <88636352+Xorol@users.noreply.github.com>
2023-06-30 01:18:37 -0400

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -25,15 +25,19 @@ KOILANG_CHANNEL_IDS = {
 KOILANG = 719617569908064348
 XOROL = 938379645919834143
 
+def load_dictionaries():
+  with open('dictionaries/dictionaries.json') as f:
+    return json.load(f)
+
 class Dictionary:
-  def __init__(self, name : str, dictionaries : dict):
+  def __init__(self, name: str, dictionaries: dict = None):
     self.file = dictionaries[name]["file"]
 
     with open(self.file) as f:
       self.raw = f.read()
 
     dictionary = dictionaries[name]
-    self.owner = dictionary['owner']
+    self.owner_id = dictionary['owner']
     self.description = dictionary['description']
     self.name = dictionary['name']
     self.display_name = dictionary['display']
@@ -42,49 +46,58 @@ class Dictionary:
 
     self.words = [i.split(',')[:5] for i in self.raw.split("\n")]
 
-  def search(self, item : str, specificity : str = "broad"):
+  @classmethod
+  async def convert(cls, name: str):
+    return cls(name, load_dictionaries())
+
+  @property
+  def length(self):
+    return len(self.words)
+
+  def search(self, item : str, specificity : str = "broad", blocks: bool = False):
     out = []
     for i in self.words:
-      out.extend(
-          i for j in i if specificity == "broad" and item in j
-          or specificity != "broad" and specificity == "narrow" and item == j)
+      match specificity:
+        case "broad" if item.lower() in ''.join(i).lower():
+          out.append(self.format_word(i))
+        case "narrow" if item.lower() == i[0].lower() or item.lower() == i[3].lower():
+          out.append(self.format_word(i))
+
     if not out:
       return "No results"
-
-    act_out = f"Result{'s' if len(out) > 1 else ''} for {item} in {self.display_name}:"
-
-    for i in out:
-      act_out += "\n" + self.format_word(i)
-
-    return act_out
+    if not blocks:
+      act_out = f"### Result{'s' if len(out) > 1 else ''} for {item} in {self.display_name}:\n" + '\n'.join(out)
+      return act_out
+    out.insert(0, f"### Result{'s' if len(out) > 1 else ''} for {item} in {self.display_name}:")
+    return out
 
   def format_word(self, word, oneline : bool = False):
     for i, j in enumerate(word[:5]):
       if (j.startswith('"') and j.endswith('"')):
         word[i] = j[1:-1]
+    if oneline:
+      return f"**{word[0]}** {'[' if word[1] else ''}{word[1]}{']' if word[1] else ''} - {word[2]}{'.' if not word[2].endswith('.') else ''} {word[3]}"
+    if len(word) < 5:
+      return f"**{word[0]}** {'[' if word[1] else ''}{word[1]}{']' if word[1] else ''}\n*{word[2]}*{'.' if not word[2].endswith('.') else ''} {word[3]}"
+    if word[4]:
+      return f"**{word[0]}** {'[' if word[1] else ''}{word[1]}{']' if word[1] else ''}\n*{word[2]}*{'.' if not word[2].endswith('.') else ''} {word[3]}{'— ' + word[4]}"
+    return f"**{word[0]}** {'[' if word[1] else ''}{word[1]}{']' if word[1] else ''}\n*{word[2]}*{'.' if not word[2].endswith('.') else ''} {word[3]}"
 
-    return (
-        f"**{word[0]}** {'/' if word[1] else ''}{word[1]}{'/' if word[1] else ''} - {word[2]}. {word[3]}"
-        if oneline else
-        f"**{word[0]}** {'/' if word[1] else ''}{word[1]}{'/' if word[1] else ''}\n*{word[2]}*. {word[3]}{f'— {word[4]}' if len(word) >= 5 else ''}"
-    )
+  def random_word(self, oneline : bool = False, raw: bool = False):
+    word = random.choice(self.words)
+    if raw:
+      return word
+    return self.format_word(word, oneline)
 
-  def random_word(self, oneline : bool = False):
-    return self.format_word(random.choice(self.words), oneline)
 
-def load_dictionaries():
-  with open('dictionaries/dictionaries.json') as f:
-    return json.load(f)
+def random_dictionary():
+  dictionaries = load_dictionaries()
+  dictionary = dictionaries[random.choice(list(dictionaries.keys()))]
+  return Dictionary(dictionary["name"], dictionaries)
 
-def generate_dictionary_choices():
-  dicts = load_dictionaries()
-  return [
-      interactions.Choice(name=dicts[i]['display'], value=dicts[i]['name'])
-      for i in dicts.keys()
-  ]
 
 async def set_game(game : str, client : interactions.Client):
-  await client.change_presence(interactions.ClientPresence(activities=[interactions.PresenceActivity(name=game, type=interactions.PresenceActivityType.GAME)]))
+  await client.change_presence(activity = game)
 
 async def get_user (id : int, client : interactions.Client):
   return interactions.User(**await client._http.get_user(id), _client=client._http)
